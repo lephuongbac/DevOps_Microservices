ae069228d6f9f314cf1d488ef2a779a592fe1b01
lephuongbac <baclp@fpt.com>
2023-06-14 00:14:49 +0700

    task 1,2,3,4

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-
+dockerpath="coderz/ml-api"
 # Step 2:  
 # Authenticate & tag
+docker login 
+docker tag ml-api:v1.0.0 $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
