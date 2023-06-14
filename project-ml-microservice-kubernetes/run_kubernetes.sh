e61768968fded21bcf6d3c086195a94440b8daa7
baclp <baclp@fpt.com>
2023-06-14 19:04:41 +0700

    tasl 5,6

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,14 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-
+docker_path=coderz/ml-api:latest
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deploy ml-api --image="$docker_path"
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward deployment.apps/ml-api 8000:80
